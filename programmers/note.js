b9b3d7be6af02a54f1db4e343f294f1f8bdc3f98
yogjin <youngjin988@gmail.com>
2022-06-25 22:01:23 +0900

    update note

--- programmers/note.js
+++ programmers/note.js
@@ -1,2 +1,20 @@
 // 1. 문자 반복 (repeat)
 console.log('*'.repeat(5)); // *****
+
+// 2. slice, splice
+
+// 3. Array 메소드
+
+// 4. swap (구조분해할당 이용)
+
+// 5. 정수 판별
+
+// 6. string 접근
+
+// 7. 제곱
+
+// 8. 진법 변환
+
+// 9. Object, Array, Map 정리
+
+// 10. Regex
